176f5910fe451d134e3b6c7cf54af261cdf29fd0
mkls <mr.antal@gmail.com>
2019-06-13 07:54:37 +0200

    third step

---
+++ src/main.spec.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const test = require('tape-promise/tape');
+// const test = require('tape');
+const escherRequest = require('./main');
+
+test('making requests', async t => {
+  // t.test('should find escher credentials based on url and set escher headers', async t => {
+  //   t.equal(1, 3);
+  //   t.end();
+  // });
+
+  t.equal(1, 1);
+  // t.end();
+});
